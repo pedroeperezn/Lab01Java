d475b22b234f47e6c8743ac0d356ab4379234148
alanbatrez <alanbatrez@hotmail.com>
2024-09-12 20:51:30 -0700

    Fixed methods
    change methods to void type

--- Lab_1b/java/Main.java
+++ Lab_1b/java/Main.java
@@ -1,12 +1,32 @@
-﻿public class Main {
+/**
+ * This is the Main class
+ * @author Pedro E. Perez & Alan Riveros
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * The constructor for the Main class
+     * @param args
+     */
     public static void main(String[] args)
     {
         // Initialize 2 objects into class Student
-        Student s1 = new Student("Tiger Woods","December 30, 1975","A00123456",true);
-        Student s2 = new Student("Bill Gates", "October 28, 1955", "A00987654",false);
+        Student s1 = new Student("Tiger Woods","A00123456","December 30, 1975",true);
+        Student s2 = new Student("Bill Gates", "A00987654", "October 28, 1955",false);
 
-        // Displaying information
-        System.out.println(s1.getName());
+        // Displaying s1 information
+        s1.getName();
+        s1.getStudentNumber();
+        s1.getDateOfBirth();
+        s1.isGraduated();
+
+        System.out.println(" ");
+
+        // Displaying s2 information
+        s2.getName();
+        s2.getStudentNumber();
+        s2.getDateOfBirth();
+        s2.isGraduated();
 
 
 
--- Lab_1b/java/Student.java
+++ Lab_1b/java/Student.java
@@ -1,7 +1,7 @@
-﻿/**
- * @author Pedro & Alan
- * @version 1.0
+/**
  * Creation of new class called Student
+ * @author Pedro E. Perez & Alan Riveros
+ * @version 1.0
  */
 public class Student
 {
@@ -27,31 +27,30 @@ public class Student
 
     /**
      * Method that is used to get the name of the Student
-     * @return Return the name of the Student
      */
-    public String getName()
+    public void getName()
     {
-        return name;
+        System.out.println(this.name);
     }
 
     /**
      * Method that is used to get the student number of the Student
-     * @return Return the student number of the Student
      */
-    public String getStudentNumber()
+    public void getStudentNumber()
     {
-        return studentNumber;
+        System.out.println("st #" + this.studentNumber);
     }
 
     /**
      * Method that is used to get the date of birth of the Student
-     * @return Return the date of birth of the Student
      */
-    public String getDateOfBirth()
-    {
-        return dateOfBirth;
+    public void getDateOfBirth() {
+        System.out.println("was born on " + this.dateOfBirth);
     }
 
+    /**
+     * Method that is used to verify if the Student is graduated or not
+     */
     public void isGraduated()
     {
         if(!this.graduated)
@@ -60,7 +59,7 @@ public class Student
         }
         else
         {
-           System.out.println("The student has graduated.");
+            System.out.println("The student has graduated.");
         }
     }
 

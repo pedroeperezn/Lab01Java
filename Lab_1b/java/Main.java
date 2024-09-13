79ecff28c6551a7a62787928883c549cda19b91c
alanbatrez <alanbatrez@hotmail.com>
2024-09-12 19:30:49 -0700

    Creation of main
    Creation of main class and initialize two instances

---
+++ Lab_1b/java/Main.java
@@ -0,0 +1,16 @@
+﻿public class Main {
+    public static void main(String[] args)
+    {
+        // Initialize 2 objects into class Student
+        Student s1 = new Student("Tiger Woods","December 30, 1975","A00123456",true);
+        Student s2 = new Student("Bill Gates", "October 28, 1955", "A00987654",false);
+
+        // Displaying information
+
+
+
+
+
+
+    }
+}
--- Lab_1b/java/Student.java
+++ Lab_1b/java/Student.java
@@ -9,6 +9,29 @@
     Student(String name,String studentNumber,String dateOfBirth,boolean graduated)
     {
         this.name = name;
+        this.studentNumber = studentNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.graduated = graduated;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getStudentNumber()
+    {
+        return studentNumber;
+    }
+
+    public String getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+
+    public boolean isGraduated()
+    {
+        return graduated;
     }
 }
 

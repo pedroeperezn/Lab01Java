25d19e8fb85878b31f269f63fc1c2329450d52fb
pedroeperezn <pedroeperezn@gmail.com>
2024-09-12 18:47:05 -0700

    Added folder for Lab1 b and started the Date class

--- First Class/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        Car  car1 = new Car("BMW","M2","Black",2020);
-        System.out.println(car1.getBrand());
-    }
-}
---
+++ Lab_1b/java/Date.java
@@ -0,0 +1,17 @@
+﻿public class Date
+{
+    private final String year;
+    private final String month;
+    private final String day;
+
+    Date(String year, String month, String day)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    
+
+
+}
